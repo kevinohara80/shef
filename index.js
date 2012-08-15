46017bf904a572067f11712348b69fb0174c586b
Kevin O'Hara <kevinohara80@gmail.com>
2012-08-14 21:13:29 -0400

    scaffolding of module

---
+++ index.js
@@ -0,0 +1,46 @@
+var request = require('request');
+var protocol = require('./lib/protocol');
+
+
+var Box = function(host, opts) {
+  if(!opts) opts = {};
+  this.host = host;
+}
+
+// get tuned information from the box
+Box.prototype.getTuned = function(opts, cb) {
+  cb();
+}
+
+Box.prototype.getProgramInfo = function(opts, cb) {
+  cb();
+}
+
+Box.prototype.tune = function(opts, cb) {
+  cb();
+}
+
+Box.prototype.key = function(key) {
+  // not sure on this api yet
+  var keyRef = {
+    up: function() {
+
+    },
+    down: function() {
+
+    },
+    press: function() {
+
+    }
+  }
+  return keyRef;
+}
+
+Box.prototype.getVersion = function(opts, cb) {
+  cb();
+}
+
+
+module.exports.createBox = function(host, opts) {
+  return new Box(host, opts);
+}
---
+++ tests/basic.js
@@ -0,0 +1,3 @@
+var test = require("tap").test
+var shef = require('../');
+
