1d358acf66a91354f2ad74df74afd60ecae4c9f4
Kevin O'Hara <kevinohara80@gmail.com>
2012-08-20 14:55:20 -0400

    updating the example

--- example/basic.js
+++ example/basic.js
@@ -1,7 +1,6 @@
 var shef = require('../');
 var box1 = shef.box('192.168.0.114');
 
-
 box1.tune(236, function(err, res) {
   if(err) {
     console.log('Err: ' + err);
@@ -9,7 +8,6 @@ box1.tune(236, function(err, res) {
     console.log(res);
   }
 });
-
 
 box1.key('list').press(function(err, res) {
   if(err) {
@@ -21,8 +19,6 @@ box1.key('list').press(function(err, res) {
     });
   }
 });
-
-
 
 box1.getProgInfo(234, function(err, res) {
   if(err) {
